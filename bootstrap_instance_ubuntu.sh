25dcb05805b55119e6dadf6887c7f03a24a25152
Jason Gilman <jason@element84.com>
2024-03-23 15:48:05 +0000

    Adding bootstrap instance ubuntu

---
+++ bootstrap_instance_ubuntu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+touch ~/.bashrc
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cat <<EOF >> ~/.bashrc
+source "$SCRIPT_DIR/shell/common_profile.sh"
+EOF
+
+sudo apt-get update
+
+sudo apt install -y \
+  build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev curl \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+curl https://pyenv.run | bash
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+pyenv install 3.10
+pyenv global 3.10
+
+cp .gitconfig ~/
+
+echo "Edit ~/.gitconfig to enter your email address if not jason@element84.com"
